54235d495cdf3b93fe5c7f68d7278d80900772b6
WaMuu0813 <1394539343@qq.com>
2026-09-07 05:55:42 -0700

    setup python3 environment

---
+++ python/day1_python_basic.py
@@ -0,0 +1,43 @@
+# 变量
+
+name = "AI Infra"
+
+year = 2026
+
+print(name)
+print(year)
+
+
+# list
+
+nums = [1,2,3,4]
+
+print(nums[0])
+
+nums.append(5)
+
+print(nums)
+
+
+#dict
+
+model = {
+    "name": "Llama",
+    "size": 7
+}
+
+print(model["name"])
+
+
+
+# 条件
+
+if year >= 2026:
+    print("start")
+
+
+
+# 循环
+
+for x in nums:
+    print(x)
